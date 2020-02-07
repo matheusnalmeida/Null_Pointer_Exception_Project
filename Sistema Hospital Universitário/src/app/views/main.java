6e036008b36da51b0a7ac35d5fba85fe0f9ef08c
Matheus Nunes <matheus.nunes00@souunit.com.br>
2020-02-07 15:06:35 -0300

    Corrigindo fxml view

---
+++ Sistema Hospital Universitário/src/app/views/main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Matheus Nunes
+ */
+public class main extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();    
+    }
+    
+}
