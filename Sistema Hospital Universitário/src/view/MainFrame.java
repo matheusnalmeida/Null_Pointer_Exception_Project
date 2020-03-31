f24175fe90229abc7935a15a88b9542bc0c61ff4
Matheus Nunes <matheus.nunes00@souunit.com.br>
2020-03-31 19:24:06 -0300

    Repaginacao da interface grafica e configuracao do projeto para uso da biblioteca jfoenix

--- Sistema Hospital Universitário/src/view/MainFrame.java
+++ Sistema Hospital Universitário/src/view/MainFrame.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
