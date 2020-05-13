41da3eaf8434d3f869c6c52620f60bc89f6efe13
Pedro Santos <pedrobsantos98@outlook.com>
2020-05-12 22:05:35 -0300

    Modificação de interfaces de gerar consultas e crud de consultas

---
+++ Sistema Hospital Universitário/src/app/controller/aluno/CrudConsultasController.java
@@ -0,0 +1,30 @@
+package app.controller.aluno;
+
+import app.view.aluno.CrudConsultas;
+import app.view.aluno.GerarConsulta;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class CrudConsultasController implements Initializable {
+
+    @FXML
+    private Button novaConsultaBotao;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void adicionarConsulta(ActionEvent evt) {
+        try {
+            GerarConsulta gerarConsulta = new GerarConsulta();
+            gerarConsulta.start(new Stage());
+            CrudConsultas.getStage().close();
+        } catch (Exception exception) {
+        }
+    }
+}
--- Sistema Hospital Universitário/src/app/model/domain/ImagemRelatorio.java
+++ Sistema Hospital Universitário/src/app/model/domain/ImagemRelatorio.java
@@ -1,7 +1,6 @@
 package app.model.domain;
 
 import java.io.Serializable;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
